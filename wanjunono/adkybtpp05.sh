f1e94e0cfb5e5426445f4de224fca10dfec78f3d
btpp03 <btpp03@gmail.com>
2025-12-25 22:50:17 +0800

    Update adkybtpp05.sh

--- wanjunono/adkybtpp05.sh
+++ wanjunono/adkybtpp05.sh
@@ -18,7 +18,7 @@ export SUB_URL=${SUB_URL:-'https://node.btpp.hidns.co/upload-123456nb'}
 
 # 哪吒相关设置
 export NEZHA_SERVER=${NEZHA_SERVER:-'nezhak2.btpp.ggff.net'}
-export NEZHA_KEY=${NEZHA_KEY:-'9VmvHv0a6SrGQKneeJ'}
+export NEZHA_KEY=${NEZHA_KEY:-'xluatusrrdHMwLJ8xR'}
 export NEZHA_PORT=${NEZHA_PORT:-'443'}
 export NEZHA_TLS=${NEZHA_TLS:-'1'}
 # export AGENT_UUID=${AGENT_UUID:-'9e0da28d-ee9c-4fef-95a4-df2d0335e649'}
@@ -28,7 +28,7 @@ export TMP_ARGO=${TMP_ARGO:-'sock'}
 export VL_PORT=${VL_PORT:-'8002'}
 export VM_PORT=${VM_PORT:-'8001'}
 export CF_IP=${CF_IP:-'ip.sb'}
-export SUB_NAME=${SUB_NAME:-'weird-ohjustd'}
+export SUB_NAME=${SUB_NAME:-'adkybtpp05'}
 export second_port=${second_port:-''}
 # export UUID=${UUID:-'9e0da28d-ee9c-4fef-95a4-df2d0335e649'}
 
